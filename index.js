92e19ff6538040b718d145ab74531ce5f67b6b35
mhuss03 <134786667+mhuss03@users.noreply.github.com>
2023-10-26 00:55:11 +0100

    Stopwatch and timer Functionality added, Just need to Clean up and finish css

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const timer = document.querySelector(".timer");
-const start = document.querySelector(".start");
-const reset = document.querySelector(".stop");
+const clock = document.querySelector(".clock");
+const start = document.querySelectorAll(".start");
+const reset = document.querySelector(".reset");
+
+const optionList = document.querySelector(".options-container");
+
+const stopwatchOption = document.querySelector(".stopwatch-option");
+const stopwatchContainer = document.querySelector(".stopwatch-container");
+
+const timerOption = document.querySelector(".timer-option");
+const timerContainer = document.querySelector(".timer-container");
+
+const fiveMin = document.querySelector(".five-min");
+const tenMin = document.querySelector(".ten-min");
 
 let state = 0;
 
@@ -8,37 +19,102 @@ let hour = 0;
 let min = 0;
 let sec = 0;
 
-timer.innerHTML = "00:00:00";
+/* *** inititalise *** */
+updateclock();
 
-start.addEventListener("click", () => {
-  if (state === 0) {
-    interval = setInterval(() => {
-      sec++;
-
-      if (sec > 59) {
-        min++;
-        sec = 0;
-      }
-      if (min > 59) {
-        hour++;
-        min = 0;
-      }
-
-      timer.innerHTML = `${hour.toString().padStart(2, "0")}:${min
-        .toString()
-        .padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
-    }, 100);
-    state = 1;
-  } else if (state === 1) {
-    clearInterval(interval);
-    state = 0;
-  }
-  console.log(state);
+/* **  Choosing Stopwatch or Timer **/
+stopwatchOption.addEventListener("click", () => {
+  optionList.classList.add("hide");
+  stopwatchContainer.classList.remove("hide");
 });
 
-reset.addEventListener("click", () => {
-  hour, min, (sec = 0);
-  timer.innerHTML = `${hour.toString().padStart(2, "0")}:${min
+timerOption.addEventListener("click", () => {
+  optionList.classList.add("hide");
+  timerContainer.classList.remove("hide");
+});
+
+/* Functions to Update count Start Stop Clock */
+function updateclock() {
+  clock.innerHTML = `${hour.toString().padStart(2, "0")}:${min
     .toString()
     .padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
+}
+
+function count(dir) {
+  if (dir === 0) {
+    sec++;
+    if (sec > 59) {
+      min++;
+      sec = 0;
+    }
+    if (min > 59) {
+      hour++;
+      min = 0;
+    }
+  } else if (dir === 1) {
+    sec--;
+    if (sec < 0) {
+      min--;
+      sec = 59;
+    }
+    if (min < 0 && hour > 0) {
+      hour--;
+      min = 59;
+    }
+  }
+}
+
+function startclock(upDown) {
+  interval = setInterval(() => {
+    count(upDown);
+    updateclock();
+  }, 1000);
+}
+
+function stopclock() {
+  clearInterval(interval);
+  updateclock();
+}
+
+/* StopWatch Start Stop Reset */
+start.forEach((button) =>
+  button.addEventListener("click", () => {
+    let upDown;
+    if (timerContainer.classList.contains("hide")) {
+      upDown = 0;
+    } else {
+      upDown = 1;
+    }
+    if (state === 0) {
+      startclock(upDown);
+      state = 1;
+      button.classList.add("off");
+      button.value = "Stop";
+    } else if (state === 1) {
+      stopclock(upDown);
+      state = 0;
+      button.classList.remove("off");
+      button.value = "Start";
+    }
+  })
+);
+
+reset.addEventListener("click", () => {
+  hour = 0;
+  min = 0;
+  sec = 0;
+  clock.innerHTML = `${hour.toString().padStart(2, "0")}:${min
+    .toString()
+    .padStart(2, "0")}:${sec.toString().padStart(2, "0")}`;
+});
+
+/* Timer 5 10 Start Clear */
+
+fiveMin.addEventListener("click", () => {
+  min += 5;
+  updateclock();
+});
+tenMin.addEventListener("click", () => {
+  min += 10;
+  updateclock();
 });
