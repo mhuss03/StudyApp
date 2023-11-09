aaf87427794c3af2f0f28b5f200be20538b1f877
Muhammad Hussain <m.hussain03@icloud.com>
2023-11-09 00:37:36 +0000

    test phase

--- index.js
+++ index.js
@@ -16,61 +16,87 @@
 const form = document.getElementById("form");
 const inputTask = form["input"];
 
-const addTask = document.getElementById("add");
+const addTask = form["add"];
 
 const taskContainer = document.getElementById("task-container");
 
-let taskData = JSON.parse(localStorage.getItem("taskData")) || [];
-
-let taskCount = 0;
+// let taskData = JSON.parse(localStorage.getItem("taskData")) || [];
 
 /*      *** Add Task  ***     */
+taskData = [];
 
 addTask.addEventListener("click", (e) => {
   e.preventDefault();
-
-  let taskstatus = 0;
-  task = inputTask.value;
-  storeData(task, taskstatus);
-
-  updateTask(task, taskstatus);
-});
-
-function storeData(task, taskstatus) {
-  taskData.push({ task, taskstatus });
-
+  taskData.push(inputTask.value);
   localStorage.setItem("taskData", JSON.stringify(taskData));
-}
 
-/*      *** Update Task Section ***     */
+  const items = JSON.parse(localStorage.getItem("taskData"));
+  for (const element of items) {
+    const div = document.createElement("div");
+    const label = document.createElement("label");
+    const del = document.createElement("button");
 
-function updateTask(task, taskstatus) {
-  const div = document.createElement("div");
-  const label = document.createElement("label");
-  const status = document.createElement("input");
-  const del = document.createElement("button");
-
-  status.type = "checkbox";
-  del.classList.add(`delete`);
-  del.classList.add(`${taskCount}`);
-
-  label.innerText = task;
-
-  div.append(label, status, del);
-  taskContainer.appendChild(div);
-
-  taskCount++;
-}
-
-taskData.forEach((element) => {
-  updateTask(element.task, element.status);
+    label.innerText = element;
+    div.append(label, del);
+    taskContainer.appendChild(div);
+  }
 });
 
-/*      *** Delete Task Section ***     */
-const deleteTask = document.querySelectorAll("delete");
+// addTask.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   let taskstatus = 0;
+//   task = inputTask.value;
+//   storeData(task, taskstatus);
 
-deleteTask.forEach((element) => {
-  element.addEventListener("click", () => {
-    element.removepare;
-  });
-});
+//   updateTask(task, taskstatus);
+// });
+
+// function storeData(task, taskstatus) {
+//   taskData.push({ task, taskstatus });
+
+//   localStorage.setItem("taskData", JSON.stringify(taskData));
+// }
+
+// /*      *** Update Task Section ***     */
+// let count = 0;
+// function updateTask(task, taskstatus) {
+//   const div = document.createElement("div");
+//   const label = document.createElement("label");
+//   const status = document.createElement("input");
+//   const del = document.createElement("button");
+
+//   status.type = "checkbox";
+//   del.classList.add(`delete`);
+//   del.classList.add(`${count}`);
+
+//   label.innerText = task;
+
+//   div.append(label, status, del);
+//   taskContainer.appendChild(div);
+//   count++;
+// }
+
+// taskData.forEach((element) => {
+//   updateTask(element.task, element.status);
+// });
+
+// /*      *** Delete Task Section ***     */
+
+// const deleteTask = document.querySelectorAll(".delete");
+
+// deleteTask.forEach((element) => {
+//   element.addEventListener("click", (e) => {
+//     e.preventDefault();
+
+//     const currentItem = JSON.parse(localStorage.getItem("taskData"));
+
+//     for (let i = 0; i < taskData.length; i++) {
+//       if (element.classList.contains(`${i}`)) {
+//         taskData.splice(i, 1);
+//       }
+//     }
+
+//     element.parentElement.remove();
+//     localStorage.setItem("taskData", JSON.stringify(taskData));
+//   });
+// });
